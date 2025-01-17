0e2cfafc993f5c0eabc87087d71701268632d93d
Prof-Immanuel <prof.immanuel2003@gmail.com>
2025-01-17 23:48:35 +0200

    fixinf build file

--- shopease/settings.py
+++ shopease/settings.py
@@ -163,12 +163,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = 'media/
 
 
 # Default primary key field type
